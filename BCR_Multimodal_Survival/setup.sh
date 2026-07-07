be36dc9d30558e2bdfc89f042047cd8a250d123f
Bogale <ybogale@iu.edu>
2026-07-07 08:36:01 -0400

    minor fix

--- BCR_Multimodal_Survival/setup.sh
+++ BCR_Multimodal_Survival/setup.sh
@@ -3,6 +3,21 @@ set -e
 
 pip install -r requirements.txt
 
+mkdir -p \
+data/raw/wsi \
+data/raw/mri \
+data/raw/clinical \
+data/reference \
+data/interim/wsi/patches \
+data/interim/wsi/all_patches \
+data/interim/wsi/embeddings \
+data/interim/wsi/cancer_only \
+data/interim/mri/embeddings \
+data/processed/pathology \
+data/processed/fusion \
+outputs/predictions \
+outputs/heatmaps
+
 # -----------------------------
 # WSITools
 # -----------------------------
